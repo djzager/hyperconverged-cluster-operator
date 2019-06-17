4faf02fb366fab33a620f62436632215099fc7ab
David Zager <david.j.zager@gmail.com>
2019-06-17 15:44:36 -0400

    Update manifest generation
    1 - default to converging the manifests (not depending on others)
    2 - if we render a CRD, add it to the olm-catalog

--- hack/build-manifests.sh
+++ hack/build-manifests.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-# TODO: If we create more hack scripts this should go in common
-# and be sourced
 PROJECT_ROOT="$(readlink -e $(dirname "$BASH_SOURCE[0]")/../)"
 
-# TODO: Move this to deploy
+CONVERGED="${CONVERGED:-true}"
 DEPLOY_DIR="${PROJECT_ROOT}/deploy"
-STD_DEPLOY_DIR="${DEPLOY_DIR}/standard"
-CONVERGED_DEPLOY_DIR="${DEPLOY_DIR}/converged"
 
 NAMESPACE="${NAMESPACE:-kubevirt-hyperconverged}"
 CSV_VERSION="${CSV_VERSION:-0.0.1}"
@@ -17,6 +13,7 @@ CNA_CONTAINER_PREFIX="${CNA_CONTAINER_PREFIX:-quay.io/kubevirt}"
 WEBUI_CONTAINER_PREFIX="${WEBUI_CONTAINER_PREFIX:-quay.io/kubevirt}"
 CONTAINER_TAG="${CONTAINER_TAG:-latest}"
 IMAGE_PULL_POLICY="${IMAGE_PULL_POLICY:-IfNotPresent}"
+CSV_DIR="olm-catalog/kubevirt-hyperconverged/${CSV_VERSION}"
 
 (cd ${PROJECT_ROOT}/tools/manifest-templator/ && go build)
 
@@ -24,46 +21,50 @@ templates=$(cd ${PROJECT_ROOT}/templates && find . -type f -name "*.yaml.in")
 for template in $templates; do
 	infile="${PROJECT_ROOT}/templates/${template}"
 
-	std_out_dir="$(dirname ${STD_DEPLOY_DIR}/${template})"
-	std_out_dir=${std_out_dir/VERSION/$CSV_VERSION}
-	mkdir -p ${std_out_dir}
-
-	std_out_file="${std_out_dir}/$(basename -s .in $template)"
-	std_out_file=${std_out_file/VERSION/v$CSV_VERSION}
-	rendered=$( \
-		${PROJECT_ROOT}/tools/manifest-templator/manifest-templator \
-		--namespace=${NAMESPACE} \
-		--csv-version=${CSV_VERSION} \
-		--container-prefix=${CONTAINER_PREFIX} \
-		--container-tag=${CONTAINER_TAG} \
-		--image-pull-policy=${IMAGE_PULL_POLICY} \
-		--input-file=${infile} \
-	)
-	if [[ ! -z "$rendered" ]]; then
-		echo -e "$rendered" > $std_out_file
+	if [ "$CONVERGED" = true ]; then
+		rendered=$( \
+			${PROJECT_ROOT}/tools/manifest-templator/manifest-templator \
+			--converged \
+			--namespace=${NAMESPACE} \
+			--csv-version=${CSV_VERSION} \
+			--container-prefix=${CONTAINER_PREFIX} \
+			--container-tag=${CONTAINER_TAG} \
+			--image-pull-policy=${IMAGE_PULL_POLICY} \
+			--input-file=${infile} \
+		)
+	else
+		# Leaving this around so it's possible to build HCO depending on
+		# other kubevirt operators
+		rendered=$( \
+			${PROJECT_ROOT}/tools/manifest-templator/manifest-templator \
+			--namespace=${NAMESPACE} \
+			--csv-version=${CSV_VERSION} \
+			--container-prefix=${CONTAINER_PREFIX} \
+			--container-tag=${CONTAINER_TAG} \
+			--image-pull-policy=${IMAGE_PULL_POLICY} \
+			--input-file=${infile} \
+		)
 	fi
 
-	converged_out_dir="$(dirname ${CONVERGED_DEPLOY_DIR}/${template})"
-	converged_out_dir=${converged_out_dir/VERSION/$CSV_VERSION}
-	mkdir -p ${converged_out_dir}
-
-	converged_out_file="${converged_out_dir}/$(basename -s .in $template)"
-	converged_out_file=${converged_out_file/VERSION/v$CSV_VERSION}
-	rendered=$( \
-		${PROJECT_ROOT}/tools/manifest-templator/manifest-templator \
-		--converged \
-		--namespace=${NAMESPACE} \
-		--csv-version=${CSV_VERSION} \
-		--container-prefix=${CONTAINER_PREFIX} \
-		--cna-container-prefix=${CNA_CONTAINER_PREFIX} \
-		--webui-container-prefix=${WEBUI_CONTAINER_PREFIX} \
-		--container-tag=${CONTAINER_TAG} \
-		--image-pull-policy=${IMAGE_PULL_POLICY} \
-		--input-file=${infile} \
-	)
+	# only write to disk if there is something to write
 	if [[ ! -z "$rendered" ]]; then
-		echo -e "$rendered" > $converged_out_file
+		out_dir="$(dirname ${DEPLOY_DIR}/${template})"
+		out_dir=${out_dir/VERSION/$CSV_VERSION}
+		mkdir -p ${out_dir}
+		out_file="${out_dir}/$(basename -s .in $template)"
+		out_file=${out_file/VERSION/$CSV_VERSION}
+
+		echo -e "$rendered" > $out_file
+		if [[ $infile =~ .*crd.yaml.in ]]; then
+			# if we have a CRD put it in the olm-catalog
+			csv_out_dir="${DEPLOY_DIR}/${CSV_DIR}"
+			mkdir -p ${csv_out_dir}
+			csv_out_file="${csv_out_dir}/$(basename -s .in $template)"
+
+			echo -e "$rendered" > $csv_out_file
+		fi
 	fi
+
 done
 
 (cd ${PROJECT_ROOT}/tools/manifest-templator/ && go clean)
